df4709114abecb8309ef4dc35ba76c2e8ac8c93e
leo <rleonel426@gmail.com>
2026-04-22 23:20:59 -0700

    added railway api

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: "leo-blog-production.up.railway.app/api",
 });
 
 export const fetchPosts = () => API.get("/posts");
